c19df70bcba3bed8609197b612b997c9c7ba3c6d
Charles Koh <charles.jcko@gmail.com>
2015-10-14 19:30:11 +0900

    first commit

---
+++ js/client.js
@@ -0,0 +1,309 @@
+
+var isChannelReady;
+var isInitiator = false;
+var isStarted = false;
+var localStream;
+var pc;
+var remoteStream;
+var turnReady;
+
+var parameters = (function () {
+  var query_string = {};
+  var query = location.search.substring(1);
+  var vars = query.split("&");
+
+  for (var i=0; i<vars.length; i++) {
+    var pair = vars[i].split('=');
+    if(typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    } else if(typeof query_string[pair[0]] === "string") {
+      var arr = [query_string[pair[0]], pair[1]];
+      query_string[pair[0]] = arr;
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  }
+  return query_string;
+})();
+
+var pc_config = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+
+var pc_constraints = {'optional': [{'DtlsSrtpKeyAgreement': true}]};
+
+// Set up audio and video regardless of what devices are present.
+var sdpConstraints = {'mandatory': {
+  'OfferToReceiveAudio':true,
+  'OfferToReceiveVideo':true }};
+
+/////////////////////////////////////////////
+
+var ROOM = parameters.stb_id;
+var USER = parameters.user_id;
+var roomOwner = null;
+
+var socket = io.connect("10.28.3.59:8081", {forceNew:true, multiplex:true});//
+
+if (ROOM !== '') {
+  console.log('join', ROOM);
+  socket.emit('join', ROOM, USER);
+}
+
+socket.on('full', function (room){
+  console.log('"' + room + '" is full');
+});
+
+socket.on('joined', function (room, owner){
+  console.log('This peer has joined room: ' + room);
+  roomOwner = owner
+  isChannelReady = true;
+});
+
+socket.on('fail', function (message){
+  console.log(message);
+});
+
+socket.on('log', function (array){
+  console.log.apply(console, array);
+});
+
+////////////////////////////////////////////////
+
+function sendMessage(message){
+
+  if (typeof message !== 'object') {
+    var msg = {
+      type : message,
+      room : ROOM,
+      to : roomOwner,
+      from : USER
+    }
+    message = msg;
+  } else {
+    message['room'] = message.room || ROOM;
+    message['to'] = message.from;
+    message['from'] = USER
+  }
+
+  console.log('Client sending message: ', message);
+  socket.emit('message', message);
+}
+
+socket.on('message', function (message){
+  if (message.to !== USER) {
+    return;
+  }
+  console.log('Client received message:', message);
+  if (message.type === 'offer') {
+    maybeStart(message.from);
+    pc.setRemoteDescription(new RTCSessionDescription(message));
+    doAnswer(message.from);
+  } else if (message.type === 'candidate') {
+    var candidate = new RTCIceCandidate({
+      sdpMLineIndex: message.label,
+      candidate: message.candidate
+    });
+    pc.addIceCandidate(candidate);
+  } else if (message === 'bye') {
+    handleRemoteHangup();
+  }
+});
+
+////////////////////////////////////////////////////
+
+var remoteVideo = document.querySelector('#remoteVideo');
+
+
+if (location.hostname != "localhost") {
+  //requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
+  //requestTurn('http://10.28.3.126:3478');
+}
+
+function maybeStart(user) {
+  if (isChannelReady) {
+    createPeerConnection(user);
+  }
+}
+
+window.onbeforeunload = function(e){
+	sendMessage('bye');
+}
+
+/////////////////////////////////////////////////////////
+
+function createPeerConnection(user) {
+  try {
+    pc = new webkitRTCPeerConnection(pc_config, pc_constraints);
+    pc.onicecandidate = handleIceCandidate.bind(this, user);
+    pc.onaddstream = handleRemoteStreamAdded;
+    pc.onremovestream = handleRemoteStreamRemoved;
+    console.log('Created RTCPeerConnnection');
+  } catch (e) {
+    console.log('Failed to create PeerConnection, exception: ' + e.message);
+    alert('Cannot create RTCPeerConnection object.');
+      return;
+  }
+}
+
+function handleIceCandidate(user, event) {
+  console.log('handleIceCandidate event: ', event);
+  if (event.candidate) {
+    sendMessage({
+      type: 'candidate',
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate,
+      from: user
+    });
+  } else {
+    console.log('End of candidates.');
+  }
+}
+
+function handleRemoteStreamAdded(event) {
+  console.log('Remote stream added.');
+  remoteVideo.src = window.URL.createObjectURL(event.stream);
+  remoteStream = event.stream;
+}
+
+function handleRemoteStreamRemoved(event) {
+  console.log('Remote stream removed. Event: ', event);
+}
+
+function doAnswer(user) {
+  console.log('Sending answer to peer.');
+  pc.createAnswer(setLocalAndSendMessage.bind(this, user), null, sdpConstraints);
+}
+
+function setLocalAndSendMessage(user, sessionDescription) {
+  // Set Opus as the preferred codec in SDP if Opus is present.
+  sessionDescription.sdp = preferOpus(sessionDescription.sdp);
+  pc.setLocalDescription(sessionDescription);
+  sendMessage({room:ROOM, type: sessionDescription.type, sdp: sessionDescription.sdp, from: user});
+}
+
+function requestTurn(turn_url) {
+  var turnExists = false;
+  for (var i in pc_config.iceServers) {
+    if (pc_config.iceServers[i].url.substr(0, 5) === 'turn:') {
+      turnExists = true;
+      turnReady = true;
+      break;
+    }
+  }
+  if (!turnExists) {
+    console.log('Getting TURN server from ', turn_url);
+    // No TURN server. Get one from computeengineondemand.appspot.com:
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var turnServer = JSON.parse(xhr.responseText);
+      	console.log('Got TURN server: ', turnServer);
+        pc_config.iceServers.push({
+          'url': 'turn:' + turnServer.username + '@' + turnServer.turn,
+          'credential': turnServer.password
+        });
+        turnReady = true;
+      }
+    };
+    xhr.open('GET', turn_url, true);
+    xhr.send();
+  }
+}
+
+function hangup() {
+  console.log('Hanging up.');
+  stop();
+  sendMessage('bye');
+}
+
+function handleRemoteHangup() {
+  console.log('Session terminated.');
+  stop();
+  isInitiator = false;
+}
+
+function stop() {
+  isStarted = false;
+  // isAudioMuted = false;
+  // isVideoMuted = false;
+  pc.close();
+  pc = null;
+}
+
+///////////////////////////////////////////
+
+// Set Opus as the default audio codec if it's present.
+function preferOpus(sdp) {
+  var sdpLines = sdp.split('\r\n');
+  var mLineIndex = null;
+  // Search for m line.
+  for (var i = 0; i < sdpLines.length; i++) {
+      if (sdpLines[i].search('m=audio') !== -1) {
+        mLineIndex = i;
+        break;
+      }
+  }
+  if (mLineIndex === null) {
+    return sdp;
+  }
+
+  // If Opus is available, set it as the default in m line.
+  for (i = 0; i < sdpLines.length; i++) {
+    if (sdpLines[i].search('opus/48000') !== -1) {
+      var opusPayload = extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
+      if (opusPayload) {
+        sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
+      }
+      break;
+    }
+  }
+
+  // Remove CN in m line and sdp.
+  sdpLines = removeCN(sdpLines, mLineIndex);
+
+  sdp = sdpLines.join('\r\n');
+  return sdp;
+}
+
+function extractSdp(sdpLine, pattern) {
+  var result = sdpLine.match(pattern);
+  return result && result.length === 2 ? result[1] : null;
+}
+
+// Set the selected codec to the first in m line.
+function setDefaultCodec(mLine, payload) {
+  var elements = mLine.split(' ');
+  var newLine = [];
+  var index = 0;
+  for (var i = 0; i < elements.length; i++) {
+    if (index === 3) { // Format of media starts from the fourth.
+      newLine[index++] = payload; // Put target payload to the first.
+    }
+    if (elements[i] !== payload) {
+      newLine[index++] = elements[i];
+    }
+  }
+  return newLine.join(' ');
+}
+
+// Strip CN from sdp before CN constraints is ready.
+function removeCN(sdpLines, mLineIndex) {
+  var mLineElements = sdpLines[mLineIndex].split(' ');
+  // Scan from end for the convenience of removing an item.
+  for (var i = sdpLines.length-1; i >= 0; i--) {
+    var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
+    if (payload) {
+      var cnPos = mLineElements.indexOf(payload);
+      if (cnPos !== -1) {
+        // Remove CN payload from m line.
+        mLineElements.splice(cnPos, 1);
+      }
+      // Remove CN line in sdp
+      sdpLines.splice(i, 1);
+    }
+  }
+
+  sdpLines[mLineIndex] = mLineElements.join(' ');
+  return sdpLines;
+}
+
---
+++ webrtc_server.js
@@ -0,0 +1,219 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(8081);
+//var uuid = require('node-uuid');
+var io = require('socket.io').listen(app);
+var LIMIT = 30;
+
+function User(name, socket) {
+  this.name = name;
+  this.socket = socket;
+}
+
+function Room(name, limit) {
+  this.name = name;
+  this.owner = null;
+  this.users = [];
+  this.userLimit = limit || LIMIT;
+  this.userCount = 0;
+}
+Room.prototype = {
+  init : function() {
+  },
+  joinRoom : function(user) {
+    var u = this.findUser(user.name);
+    if (u == null) {
+      this.users.push(user);
+      this.userCount++;
+    } else {
+      u.name = user.name;
+      u.socket = user.socket;
+    }
+  },
+  findUser : function(name) {
+    var ul = this.users.filter(function (e) {
+      return e.name === name;
+    });
+
+    return (ul.length > 0) ? ul[0] : null;
+  },
+  findUserBySocket : function(socket) {
+    var ul = this.users.filter(function (e) {
+      return e.socket.id === socket.id;
+    });
+
+    return (ul.length > 0) ? ul[0] : null;
+  },
+  leaveRoom : function(name) {
+    var self = this;
+    this.users.forEach(function(e,i,v){
+      if (e.name === name) {
+        delete self.users[i];
+        self.users.splice(i,1);
+        self.userCount--;
+        return;
+      }
+    });
+  },
+  leaveRoomBySocket : function(socket) {
+    var self = this;
+    this.users.forEach(function(e,i,v){
+      if (e.socket.id === socket.id) {
+        delete self.users[i];
+        self.users.splice(i,1);
+        self.userCount--;
+        return;
+      }
+    });
+  },
+  leaveAll : function() {
+    var self = this;
+    this.users.forEach(function(e,i,v){
+      e.socket.leave(self.name, function(){});
+      delete self.users[i];
+    });
+    this.users = [];
+    this.userCount = 0;
+  }
+};
+
+
+function RoomManager() {
+  this.rooms = [];
+} 
+RoomManager.prototype = {
+  init: function() {
+  },
+  openRoom: function(name, limit) {
+    var r = this.rooms.filter(function(e) {
+      return e.name === name;
+    });
+    if (r.length == 0) {
+      r = new Room(name, limit);
+      this.rooms.push(r);
+    } else {
+      return r[0];
+    }
+    return r;
+  },
+  findRoom: function(name) {
+    var r = this.rooms.filter(function(e) {
+      return e.name === name;
+    });
+    return (r.length == 0) ? null : r[0];
+  },
+  closeRoom: function(name) {
+    var self = this;
+    this.rooms.forEach(function(e,i,v) {
+      if (e.name == name) {
+        e.leaveAll();
+        delete self.rooms[i];
+        self.rooms.splice(i,1);
+        return;
+      }
+    });
+  },
+  addUser: function(socket, roomName, userName, isOwner) {
+    var r = this.findRoom(roomName);
+    if (r) {
+      var u = new User(userName, socket);
+      r.joinRoom(u);
+      isOwner && (r.owner = userName);
+    } else {
+      console.log("not found Room (" + roomName + ")");
+      return false;
+    }
+    return true;
+  },
+  removeUser: function(socket) {
+    var r = this.findRoom(socket.room);
+    if (r) {
+      r.leaveRoomBySocket(socket);
+    }
+  }
+};
+
+var rm = new RoomManager();
+
+io.sockets.on('connection', function (socket){
+
+  // 로그 서버와 클라이언트로 메세지를 전송하는 편의 함수
+  function log(){
+    var array = ['>>> Message from server: '];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+      socket.emit('log', array);
+  }
+
+  socket.on('message', function (message) {
+    log('Got message:', message);
+    //socket.broadcast.to(message.room).emit('message', message);  // other sockets
+    //io.sockets.in(message.room).emit('message', message);  // all sockets
+    var r = rm.findRoom(message.room);
+    var u = r && r.findUser(message.to);
+    if (u) {
+      //u.socket.emit(message);
+      socket.broadcast.to(message.room).emit('message', message);
+    }
+  });
+
+  socket.on('create', function (room, limit, user) {
+    var client = io.of('/').adapter['rooms'][room];
+
+    log('Request to create room ' + room);
+    socket.room = room;
+    if (!client){
+      rm.openRoom(room, limit);
+      rm.addUser(socket, room, user, true);
+      socket.join(room);
+      socket.emit('created', room, user);
+    } else {
+      socket.emit('fail', '(' + room + ') is already exist.');
+    }
+  });
+
+  socket.on('join', function (room, user) {
+    var numClients = 0;
+    var client = io.of('/').adapter['rooms'][room];
+
+    var r = rm.findRoom(room);
+
+    if (!r) {
+      socket.emit('fail', 'not found room(' + room + ')');
+      return;
+    } 
+
+    if (io.sockets.clients) {
+      numClients = io.sockets.clients(room).length;
+    }
+    socket.room = room;
+    if (r.userCount < r.userLimit) {
+      rm.addUser(socket, room, user);
+      socket.join(room);
+      socket.broadcast.to(room).emit('join', room, user, numClients);
+      socket.emit('joined', room, r.owner);
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    var r = rm.findRoom(socket.room);
+    var ro = r && r.owner;
+    var so = r && r.findUserBySocket(socket);
+    if (so && ro === so.name) {
+      rm.closeRoom(socket.room);
+    } else {
+      rm.removeUser(socket);
+    }
+    socket.leave(socket.room, function() {
+    console.log('This room is closed! : ' + socket.id);
+   });
+  });
+
+});
+
